a5576bbfb85251492e76b72e76165954f14d867f
eMobilis <ryankedi305@gmail.com>
2023-02-09 09:15:25 +0300

    ifstatements

---
+++ conditions/ifstatements.py
@@ -0,0 +1,19 @@
+code = int(input("Enter valid MMI code"))
+
+if code == 1:
+    print("data deals")
+
+elif code == 2 :
+    print( "daily bundles")
+
+elif code == 3 :
+    print("weekly bundles")
+
+elif code == 4 :
+    print("GO MONTHLY")
+
+elif code == 5 :
+    print("no expiry")
+
+else:
+    print("MORE")
---
+++ datastructures/dictionaries.py
@@ -0,0 +1,4 @@
+
+friends = {"loui" : 25, "lissa" : 22, "rita" : 26}
+friends["joe"] = 23
+print(friends)
