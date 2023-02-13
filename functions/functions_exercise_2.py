7b760c49ce5ac8b281a22856401bcf547cbbcf3b
eMobilis <ryankedi305@gmail.com>
2023-02-13 09:10:25 +0300

    first update

---
+++ functions/functions_exercise_2.py
@@ -0,0 +1,8 @@
+def multiplytwonumbers():
+    num1 = int(input("Enter first number"))
+    num2 = int(input("Enter second number"))
+
+    product = num1 * num2
+    print(f"The product is {product}")
+
+multiplytwonumbers()
