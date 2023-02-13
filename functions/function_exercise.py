c8ce06e842f68fe4a3e418eee08bd5032b2a678f
eMobilis <ryankedi305@gmail.com>
2023-02-13 08:51:59 +0300

    functions

---
+++ functions/function_exercise.py
@@ -0,0 +1,6 @@
+def calculate_months():
+    age = int(input("Enter your age"))
+    months_lived = age * 12
+    print(f"You have lived {months_lived} months")
+
+calculate_months()    
---
+++ functions/functions.py
@@ -0,0 +1,10 @@
+def sayhello():
+    print("Do you want to switch off the machine")
+
+sayhello()
+
+def restartmachine():
+    print("Restart machine")
+
+restartmachine()
+    
