aa76fd736ae1f5cd05569812cabeb8c5043c5b7f
eMobilis <ryankedi305@gmail.com>
2023-02-15 09:08:14 +0300

    blueprint

---
+++ oops/house_blueprint.py
@@ -0,0 +1,72 @@
+class Vehicle():
+    def __int__(self, owner, colour, price, type):
+        self.owner = owner
+        self.colour = colour
+        self.price = price
+        self.type = type
+
+
+vehicle_1 = Vehicle("Kedi", "black", "7M", "Hayabusa")
+print(vehicle_1.owner)
+print(vehicle_1.colour)
+print(vehicle_1.price)
+print(vehicle_1.type)
+
+print()
+
+vehicle_2 = Vehicle("Fathi", "blue", "6.5M", "r1m")
+print(vehicle_2.owner)
+print(vehicle_2.colour)
+print(vehicle_2.price)
+print(vehicle_2.type)
+
+print("---------------------vehicle_2-------------------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vehicle_3 = Vehicle("Lawri", "Red", "6M", "supra")
+print(vehicle_3.owner)
+print(vehicle_3.colour)
+print(vehicle_3.price)
+print(vehicle_3.type)
